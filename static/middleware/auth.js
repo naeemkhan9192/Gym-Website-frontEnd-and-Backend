ac6edc4197d5ba49e008a6c6511c56cb777f2ca9
Naeem <sweetdreams9192@gmail.com>
2021-09-09 12:17:05 +0500

    added the env file that manage the secrete ke

--- static/middleware/auth.js
+++ static/middleware/auth.js
@@ -4,7 +4,7 @@ const userRegister = require('../db/signup');
 const auth = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
-        const verifyUser = jwt.verify(token, 'mynameisnaeemullahandiamawebdeveloperigrandsoon');
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
         const user = await userRegister.findOne({ _id: verifyUser._id });
         req.token = token;
         req.user = user;
